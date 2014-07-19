b6487ce74e068abfbfb4658d13f39b79f39a9f72
Nick Kohrn <bnkohrn@gmail.com>
2014-07-19 16:55:02 -0400

    Initial commit

---
+++ CrystalBall/CrystalBall.swift
@@ -0,0 +1,59 @@
+//
+//  CrystalBall.swift
+//  CrystalBall
+//
+//  Created by Nick Kohrn on 7/19/14.
+//  Copyright (c) 2014 Nick Kohrn. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CrystalBall: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet var newAnswerTextField : UITextField
+    @IBOutlet var alertButton : UIButton
+    @IBOutlet var answerLabel : UILabel
+    var answers = ["Yes", "No", "Maybe"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var buttonTap = UITapGestureRecognizer(target: self, action: "showAlert")
+        self.alertButton.addGestureRecognizer(buttonTap)
+        newAnswerTextField.delegate = self
+    }
+    
+    override func canBecomeFirstResponder() -> Bool {
+        return true
+    }
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent!) {
+        if (motion == UIEventSubtype.MotionShake){
+            updateAnswer()
+        }
+    }
+    
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        var newAnswer = textField.text
+        answers.append(newAnswer)
+        textField.text = ""
+        textField.resignFirstResponder()
+        return false
+    }
+    
+    func randomAnswer() -> String {
+        var randomAnswerIndex = arc4random_uniform(UInt32(answers.count))
+        return answers[Int(randomAnswerIndex)]
+    }
+    
+    func updateAnswer() {
+        answerLabel.text = randomAnswer()
+    }
+    
+    func showAlert() {
+        var alert = UIAlertController(title: "Answer", message: randomAnswer(), preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "Click", style: UIAlertActionStyle.Default, handler: nil))
+        presentViewController(alert, animated: true, completion: nil)
+    }
+    
+}
---
+++ CrystalBall/MyPlayground.playground/section-1.swift
@@ -0,0 +1,8 @@
+// Playground - noun: a place where people can play
+
+import Cocoa
+import Foundation
+
+var answers = ["Yes", "No", "Maybe"]
+var randomAnswerIndex = arc4random_uniform(UInt32(answers.count))]
+return answers[randomAnswerIndex]
